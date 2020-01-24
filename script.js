7c7f680b591c5c535d6a377fc6f687ec0e21a608
phatcharapol-dev <phatcharapol.dev@gmail.com>
2020-01-24 18:29:16 +0700

    edit file some feature

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
- // Landing Page
-	$('a[href^="#"]').on('click', function(event) {
-	var target = $( $(this).attr('href') );
-	if( target.length ) {
-		event.preventDefault();
-		$('html, body').animate({
-			scrollTop: target.offset().top-80,
-			scrollBottom: target.offset().bottom
-		}, 800);
-	} });
-	
-	//Button porfolio
-	var btnfull = document.getElementsByClassName('btn-full') ;
-	for (var i = 0; i < btnfull.length; i++) {
-	btnfull[i].addEventListener("click", function() {
-		var current = document.getElementsByClassName("btn-full active");
-		current[0].className = current[0].className.replace(" active", "");
-		this.className += " active";
-	});
-	}
-	// init Isotope
-	var $grid = $('.grid').isotope({
-	// options
-	});
-	// filter items on button click
-	$('.filter-button-group').on( 'click', 'button', function() {
-	var filterValue = $(this).attr('data-filter');
-	$grid.isotope({ filter: filterValue });
-	});
-	// filter ProductD
-	$grid.isotope({ filter: '.productD' });
-	// filter ProductC
-	$grid.isotope({ filter: '.productC' });
+	// Landing Page
+$('a[href^="#"]').on('click', function(event) {
+var target = $( $(this).attr('href') );
+if( target.length ) {
+	event.preventDefault();
+	$('html, body').animate({
+		scrollTop: target.offset().top-80,
+		scrollBottom: target.offset().bottom
+	}, 800);
+} });
 
-	// filter ProductB
-	$grid.isotope({ filter: '.productB' });
+//Button porfolio
+var btnfull = document.getElementsByClassName('btn-full') ;
+for (var i = 0; i < btnfull.length; i++) {
+btnfull[i].addEventListener("click", function() {
+	var current = document.getElementsByClassName("btn-full active");
+	current[0].className = current[0].className.replace(" active", "");
+	this.className += " active";
+});
+}
+// Initial Isotope
+var $grid = $('.grid').isotope({
+// options
+});
+// Filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+var filterValue = $(this).attr('data-filter');
+$grid.isotope({ filter: filterValue });
+});
+// filter ProductD
+$grid.isotope({ filter: '.productD' });
+// filter ProductC
+$grid.isotope({ filter: '.productC' });
 
-	// filter .ProductA
-	$grid.isotope({ filter: '.productA' });
+// filter ProductB
+$grid.isotope({ filter: '.productB' });
+
+// filter .ProductA
+$grid.isotope({ filter: '.productA' });
+
+// Carousel auto off
+$('.carousel').carousel('pause');
+
 
